3b164a65eb4d3a6f27ed76965a5c0e9dd5069331
puzb <60679648+puzb@users.noreply.github.com>
2020-03-13 22:41:47 +0800

    全国数据 数据库访问

--- virus_web/javasrc/dao/ProvinceDAOImpl.java
+++ virus_web/javasrc/dao/ProvinceDAOImpl.java
@@ -1,1 +1,35 @@
-sdasda
+package dao;
+import pojo.Province;
+import util.DBUtil;
+import java.sql.*;
+
+
+public class ProvinceDAOImpl implements ProvinceDAO{
+	@Override
+	public Province getNationData() {
+		String sql = "select * from 全国 where date = '2020.02.02'";
+		Province pce = new Province();
+        try (Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);ResultSet rs = ps.executeQuery();) {
+		    while(rs.next()){
+		        int ipnum = rs.getInt("ipcount");
+		        int spnum = rs.getInt("spcount");
+		        int curenum = rs.getInt("curecount");
+		        int deadnum = rs.getInt("deadcount");
+		        pce.setipcount(ipnum);
+		        pce.setspcount(spnum);
+		        pce.setcurecount(curenum);
+		        pce.setdeadcount(deadnum);              									
+		     }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+		return pce;
+	}
+
+	@Override
+	public Province getData(String Pname, String date) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
