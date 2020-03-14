657dee0353bcb23c0b76db8a2567125850e97d5c
puzb <60679648+puzb@users.noreply.github.com>
2020-03-14 12:37:08 +0800

    选择日期

--- virus_web/javasrc/servlet/dateservlet.java
+++ virus_web/javasrc/servlet/dateservlet.java
@@ -1,1 +1,54 @@
-dd
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import dao.ProvinceDAO;
+import dao.ProvinceDAOImpl;
+import pojo.Province;
+
+
+/**
+ * Servlet implementation class provinServlet
+ */
+@WebServlet("/dateServlet")
+public class dateServlet extends HttpServlet {
+	private ProvinceDAO provinceDAO = new ProvinceDAOImpl();
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public dateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		String date = request.getParameter("date");
+		Province nation = getNationData(date);
+		request.setAttribute("nation", nation);
+		request.getRequestDispatcher("index.jsp").forward(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	private Province getNationData(String str) {
+		Province pce = provinceDAO.getNationData(str);
+		return pce;
+	}
+
+}
